d3a95f004856cef79961d48e8b49477d653663b8
Lucas Nehme <lucasnehme8@gmail.com>
2023-09-26 22:22:03 -0300

    cambios martes

--- playlist.js
+++ playlist.js
@@ -1,14 +1,24 @@
-// Obtén referencias a los elementos HTML
-const fileInput = document.getElementById('fileInput');
-const audioPlayer = document.getElementById('audioPlayer');
+document.addEventListener("DOMContentLoaded", function () {
+    const songList = document.getElementById("song-list");
+    const addSongButton = document.getElementById("add-button");
 
-// Agrega un evento 'change' al input de archivo
-fileInput.addEventListener('change', function() {
-    const selectedFile = fileInput.files[0];
+    addSongButton.addEventListener("click", function () {
+        const songName = document.getElementById("song-name").value;
+        const artist = document.getElementById("artist").value;
+        const songFile = document.getElementById("song-file").files[0];
 
-    if (selectedFile) {
-        const objectURL = URL.createObjectURL(selectedFile);
-        audioPlayer.src = objectURL;
-        audioPlayer.play();
-    }
+        if (songName && artist && songFile) {
+            const listItem = document.createElement("li");
+            listItem.textContent = `${songName} - ${artist}`;
+            songList.appendChild(listItem);
+
+            // Aquí puedes cargar el archivo de audio a tu servidor o realizar otras acciones necesarias
+            // Para este ejemplo, simplemente mostramos el nombre del archivo seleccionado
+            console.log("Archivo seleccionado:", songFile.name);
+
+            document.getElementById("song-name").value = "";
+            document.getElementById("artist").value = "";
+            document.getElementById("song-file").value = "";
+        }
+    });
 });
